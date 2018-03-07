c749f6cbd52c699c1c8c33fcd9bf195cb023803c
calvinoneill96 <35769325+calvinoneill96@users.noreply.github.com>
2018-03-07 14:17:18 +0000

    Display winning message

--- js/breakout.js
+++ js/breakout.js
@@ -152,6 +152,10 @@ function collisionDetection() {
 				dy = -dy;
 				b.status = 0;
 				score++;
+				if(score == brickRowCount*brickColumnCount) {
+					alert("YOU WIN, CONGRATULATIONS!");
+					document.location.reload();
+				}
 				}
 			}
 		}
